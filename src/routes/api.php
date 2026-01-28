6113544b7095155485d4e83fa2e6f169a92c5768
channyengcambo <channyengcambodia@gmail.com>
2026-01-28 21:28:40 +0700

    Add role manager and role staff

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,24 +20,29 @@ Route::middleware(['audit'])->group(function () {
                 Route::delete("/navigation-menu-items/{id}", [NavigationMenuItemsController::class, 'destroy']);
                 Route::delete("/main-heroes/{id}", [MainHeroController::class, 'destroy']);
             });
+
+//            Role Manager
+            Route::middleware(['role:MANAGER,ADMIN'])->group(function () {
+                Route::put("/navigation-menu-items/{id}", [NavigationMenuItemsController::class, 'update']);
+                Route::post("/main-heroes/{id}", [MainHeroController::class, 'updateMainHero']);
+            });
+
+//            Role Staff
+            Route::middleware(['role:ADMIN,MANAGER,STAFF'])->group(function () {
+                // Navigation Menu
+                Route::post("/navigation-menu-items", [NavigationMenuItemsController::class, 'store']);
+                Route::post('/navigation-menu-items/{parentId}/child', [NavigationMenuItemsController::class, 'storeChild']);
+
+                //    Main Hero
+                Route::post("/main-heroes", [MainHeroController::class, 'store']);
+            });
 //        Auth
             Route::post('/auth/logout', [AuthController::class, 'logout']);
             Route::post('/auth/refresh', [AuthController::class, 'refresh']);
             Route::post('/auth/log-out', [AuthController::class, 'logout']);
             Route::post('/auth/log-out-all', [AuthController::class, 'logoutAll']);
-
-//        User
             Route::get('/me', [UserController::class, 'me']);
-
-//        Navigation Menu
-            Route::post("/navigation-menu-items", [NavigationMenuItemsController::class, 'store']);
-            Route::post('/navigation-menu-items/{parentId}/child', [NavigationMenuItemsController::class, 'storeChild']);
-            Route::put("/navigation-menu-items/{id}", [NavigationMenuItemsController::class, 'update']);
         });
-
-//    Main Hero
-        Route::post("/main-heroes", [MainHeroController::class, 'store']);
-        Route::post("/main-heroes/{id}", [MainHeroController::class, 'updateMainHero']);
     });
 
 //Public
